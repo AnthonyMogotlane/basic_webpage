75c1ba12fdcd0726bc2aee46f696313d666e110b
Anthony <almogotlane@gmail.com>
2022-03-30 09:54:31 +0200

    update file

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const scrollBtn = document.querySelector(".scroll-up");
 const navList = document.querySelector(".nav-list");
 const menuBtn = document.querySelector(".menu-btn");
+const navItem = document.getElementsByClassName("nav-item");
+const menuClose = document.querySelector(".menu-close");
 
-scrollBtn.addEventListener("click", scrollTop = () => {
+scrollBtn.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
     left: 0,
@@ -13,5 +15,28 @@ scrollBtn.addEventListener("click", scrollTop = () => {
 
 menuBtn.addEventListener("click", () => {
   navList.classList.toggle("nav-show");
-  navList.classList.toggle("nav-list")
+  menuBtn.style.display = "none";
+  menuClose.style.display = "block"
 })
+
+navItem[0].addEventListener("click", () => {
+  navList.classList.remove("nav-show");
+  menuClose.classList.toggle("nav-hide")
+})
+navItem[1].addEventListener("click", () => {
+  navList.classList.remove("nav-show");
+})
+navItem[2].addEventListener("click", () => {
+  navList.classList.remove("nav-show");
+})
+navItem[3].addEventListener("click", () => {
+  navList.classList.remove("nav-show");
+})
+
+menuClose.addEventListener("click", () => {
+  menuBtn.style.display = "block";
+  menuClose.style.display = "none";
+  navList.classList.remove("nav-show");
+})
+
+
